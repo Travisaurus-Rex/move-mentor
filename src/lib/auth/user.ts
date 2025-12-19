2b8b8dcf9f049401e17da7a17a71d9a018546433
Travis Adams <brokenCD@live.com>
2025-12-19 18:57:04 +0700

    fix the broken c/p indent

--- src/lib/auth/user.ts
+++ src/lib/auth/user.ts
@@ -3,18 +3,18 @@ import { prisma } from "../prisma"
 import { User } from "@/generated/prisma/client"
 
 export async function getUser(): Promise<User> {
-    const session = await getServerSession()
+    const session = await getServerSession();
     
     if (!session?.user?.email) {
-    throw new Error('Not authenticated')
+        throw new Error('Not authenticated');
     }
 
     const user = await prisma.user.findUnique({
-    where: { email: session.user.email },
-    })
+        where: { email: session.user.email },
+    });
 
     if (!user) {
-    throw new Error('User not found')
+        throw new Error('User not found');
     }
 
     return user;
